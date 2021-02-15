4662fca7821ab11fbba4af78add580286d0543f4
Kayla Huang <kayla@MacBook-Pro-5.local>
2021-02-15 16:16:38 -0500

    python backend

---
+++ api/api.py
@@ -0,0 +1,11 @@
+# export FLASK_APP=api.py
+# apparently .flaskenv does not run even though i have installed python-dotenv
+
+import time
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
